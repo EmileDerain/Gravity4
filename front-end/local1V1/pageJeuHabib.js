bfaaf119a1453ce0436d2b6fdefd1b05515d0bf8
habib belmokhtar <habib.belmokhtar@etu.unice.fr>
2023-01-30 16:37:29 +0100

    la grille en grid

--- front-end/local1V1/pageJeuHabib.js
+++ front-end/local1V1/pageJeuHabib.js
@@ -207,3 +207,45 @@ function isThatPlayOKay(int) {
     return true;
 }
 
+window.onload = function() {
+
+    const temps = document.getElementById("timer")
+    let tempo = 0;
+
+    function timer() {
+        let minutes = parseInt(tempo / 60, 10)
+        let secondes = parseInt(tempo % 60, 10)
+        tempo++
+        let textMinutes = (minutes>9)?minutes:"0"+minutes;
+        let textSecondes = (secondes>9)?secondes:"0"+secondes;
+        temps.innerText = textMinutes + ":" + textSecondes;
+    }
+
+    function colorieBasPlateau(){
+        let tab = [];
+        tab.push(document.getElementById("column1"));
+        tab.push(document.getElementById("column2"));
+        tab.push(document.getElementById("column3"));
+        tab.push(document.getElementById("column4"));
+        tab.push(document.getElementById("column5"));
+        tab.push(document.getElementById("column6"));
+        tab.push(document.getElementById("column7"));
+        tab.forEach(x => colorie(x));
+    }
+    function colorie(elements){
+        let taille = elements.length
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener("mouseover", function() {
+
+                elements[taille-1].style.border = "5px #0059ff inset";
+            });
+            elements[i].addEventListener("mouseout", function() {
+                elements[taille-1].style.border = "5px #00ff15 inset";
+            });
+        }
+    }
+
+    setInterval(timer, 1000);
+    colorieBasPlateau();
+}
+
