bfcffa4c885c62bc5a1ff55e82d33cedcad33f03
emile <emilederain@gmail.com>
2023-02-12 19:03:01 +0100

    Il y a un bug avec git

--- front-end/navigate.js
+++ front-end/navigate.js
@@ -1,10 +1,25 @@
 function showPlayPageNC(){
-    console.log("goTo");
     document.location.href = "/homepages/homepageSelectionNC.html";
-    console.log(document.location);
+}
+
+function showPlayPageC(){
+    document.location.href = "/homepages/homepageSelectionC.html";
 }
 
 function showHomepageNC(){
-    console.log("go");
     document.location.href = "/homepages/homepage.html";
 }
+
+function start1vs1Local(){
+    document.location.href = "/mylocal1v1/pageJeu.html";
+}
+
+function startIAGame(){
+    document.location.href = "/IAGame/pageJeuHabib.html";
+}
+
+function showHomepageC(){
+    console.log("go");
+    document.location.href = "/homepages/homepageC.html";
+}
+
